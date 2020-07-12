560694e1501d9835086120f03e36e85e753dec15
Bobby Roe <bobbyroe@gmail.com>
2020-07-11 21:57:31 -0700

    clean-up + squash and strech

--- robotbobby/tutorials/bouncyball/ball.js
+++ robotbobby/tutorials/bouncyball/ball.js
@@ -3,11 +3,9 @@ canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
 const ctx = canvas.getContext('2d');
 // offscreen canvas
-const balls = ['🍊', '🤪', '🥶', '🕸', '🦴', '🌈'];
-let ballIndex = 0;
 function fillBall() {
   offCtx.clearRect(0, 0, offscreen.width, offscreen.height);
-  offCtx.fillText(balls[ballIndex], 100, 100);
+  offCtx.fillText('🍊', 100, 100);
 }
 const offscreen = document.createElement('canvas');
 const canvasSize = 200;
@@ -18,19 +16,24 @@ offCtx.font = '150px serif';
 offCtx.textAlign = 'center';
 offCtx.textBaseline = 'middle';
 fillBall();
+ctx.strokeStyle = '#FF0000';
 
 const stageRect = canvas.getBoundingClientRect();
 const pos = { x: stageRect.width * 0.5, y: stageRect.height * 0.5 };
 const scale = { x: 1, y: 1 };
 const velocityMult = 10;
-const velocity = { x: Math.random() * velocityMult, y: Math.random() * velocityMult };
+const velocity = {
+  x: 0, // Math.random() * velocityMult - velocityMult * 0.5,
+  y: 0, // Math.random() * velocityMult - velocityMult * 0.5,
+};
 const radius = 60;
 const gravity = 1.05;
 let isDragging = false;
 let isPaused = false;
 let mousePos = {};
-const drag = 0.999;
-let goalScale = 1;
+const drag = 0.99;
+const goalScale = { x: 1, y: 1 };
+const scaleFactor = { x: 1, y: 1 };
 function loop() {
   requestAnimationFrame(loop);
   if (isPaused) {
@@ -51,17 +54,30 @@ function loop() {
     velocity.y *= drag;
   }
 
-  ctx.fillStyle = 'rgba(0,0,0,0.001)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'rgba(0,0,0,0.05)';
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = 'rgba(0,0,0,1)';
-  ctx.drawImage(offscreen, pos.x - 100, pos.y - 100, canvasSize * scale.x, canvasSize * scale.y);
+  ctx.translate(pos.x, pos.y);
+  ctx.translate(-canvasSize * scale.x * 0.5, -canvasSize * scale.y * 0.5);
+  ctx.drawImage(offscreen, 0, 0, canvasSize * scale.x, canvasSize * scale.y);
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
 
   // screen bounds
-  if (pos.x + velocity.x > stageRect.width - radius || pos.x + velocity.x < radius) {
+  const hasHitRightEdge = pos.x + velocity.x > stageRect.width - radius;
+  if (hasHitRightEdge || pos.x + velocity.x < radius) {
+    scaleFactor.x = Math.min(0.5, Math.max((Math.abs(velocity.x) - 15) * 0.05, 0));
+    scale.y = 1 + scaleFactor.x;
+    scale.x = 1 - scaleFactor.x;
+    // reverse direction
     velocity.x *= -1;
   }
   const hasHitBottom = pos.y + velocity.y > stageRect.height - radius;
   if (hasHitBottom || pos.y + velocity.y < radius) {
+    scaleFactor.y = Math.min(0.5, Math.max((Math.abs(velocity.y) - 15) * 0.05, 0));
+    scale.y = 1 - scaleFactor.y;
+    scale.x = 1 + scaleFactor.y;
+    // reverse direction
     velocity.y *= -1;
   }
 
@@ -70,24 +86,12 @@ function loop() {
     velocity.y += gravity;
   }
 
-  const random = Math.random();
-  if (random < 0.05) {
-    goalScale = Math.random() * 3 + 1;
-  }
-  scale.x -= (scale.x - goalScale) * 0.05;
-  scale.y -= (scale.y - goalScale) * 0.05;
-}
-function incrementBallIndex() {
-  ballIndex += 1;
-  if (ballIndex >= balls.length) {
-    ballIndex = 0;
-  }
-}
-function decrementBallIndex() {
-  ballIndex -= 1;
-  if (ballIndex < 0) {
-    ballIndex = balls.length - 1;
-  }
+  // const random = Math.random();
+  // if (random < 0.05) {
+  //   goalScale = Math.random() * 3 + 1;
+  // }
+  scale.x -= (scale.x - goalScale.x) * 0.3;
+  scale.y -= (scale.y - goalScale.y) * 0.3;
 }
 // click and keyboard event listeners
 document.body.addEventListener('keydown', (event) => {
@@ -95,26 +99,16 @@ document.body.addEventListener('keydown', (event) => {
   if (key === 'Escape') {
     isPaused = !isPaused;
   }
-  // change balls
-  if (key === 'ArrowUp') {
-    incrementBallIndex();
-    fillBall();
-  }
-  if (key === 'ArrowDown') {
-    decrementBallIndex();
-    fillBall();
-  }
 });
 
 document.body.addEventListener('mousedown', (event) => {
   const { x, y } = event;
   const clickedDistance = (x - pos.x) ** 2 + (y - pos.y) ** 2;
-  const isClicked = clickedDistance < radius ** 2;
+  const isClicked = clickedDistance < (radius * scale.x) ** 2;
   if (isClicked) {
     isPaused = false;
     isDragging = true;
   }
-  console.log(Math.sqrt(clickedDistance));
 });
 
 document.body.addEventListener('mouseup', (event) => {
@@ -131,3 +125,9 @@ document.body.addEventListener('mousemove', (event) => {
 
 // start!
 loop();
+
+// TODOs:
+// add support for touch events
+// add squash and stretch
+// add painted walls marks
+// is this project about canvas drawing? or physics?
